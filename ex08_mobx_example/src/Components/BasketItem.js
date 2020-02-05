911189eb6c284efae380d7f411e23627b72730ca
soobinnn <isb9082@naver.com>
2020-02-05 17:08:30 +0900

    컴포넌트 최적화 개선

--- ex08_mobx_example/src/Components/BasketItem.js
+++ ex08_mobx_example/src/Components/BasketItem.js
@@ -2,13 +2,15 @@ import React from 'react';
 import '../Resource/css/BasketItem.css';
 import { observer } from 'mobx-react';
 
-const BasketItem = ({ name, price, count }) => {
+const BasketItem = ({ item, onTake }) => {
 	return (
 		<div className='BasketItem'>
-			<div className='name'>{name}</div>
-			<div className='price'>{price}원</div>
-			<div className='count'>{count}</div>
-			<div className='return'>갖다놓기</div>
+			<div className='name'>{item.name}</div>
+			<div className='price'>{item.price}원</div>
+			<div className='count'>{item.count}</div>
+			<div className='return' onClick={() => onTake(item.name)}>
+				갖다놓기
+			</div>
 		</div>
 	);
 };
