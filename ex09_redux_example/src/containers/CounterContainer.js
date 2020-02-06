61da2dee61592753bf2049558ec39879bf459cdd
soobinnn <isb9082@naver.com>
2020-02-06 16:14:00 +0900

    add redux example1

---
+++ ex09_redux_example/src/containers/CounterContainer.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { bindActionCreators } from 'redux';
+import { increment, decrement } from '../store/modules/counter';
+
+class CounterContainer extends Component {
+  handleIncrement = () => {
+    this.props.increment();
+  };
+  handleDecrement = () => {
+    this.props.decrement();
+  };
+  render() {
+    const { color, number } = this.props;
+    return (
+      <Counter
+        color={color}
+        value={number}
+        onIncrement={this.handleIncrement}
+        onDecrement={this.handleDecrement}
+      />
+    );
+  }
+}
+
+const mapStateToProps = ({ counter }) => ({
+  color: counter.color,
+  number: counter.number,
+});
+
+// bindActionCreators은 액션이 두개 이상일 때,
+//actionCreator: (...params) => dispatch(actionCreator(...params)에 해당하는 작업을 자동으로 해줌. 만약 파라미터를 필요로 하는 것이더라도 정상적으로 작동함.
+
+// 함수형태가 아닌 액션생성함수로 이뤄진 객체를 전달해주면 connect가 발생하게 될 때 bindACtionCreators를 자동으로 해줌.
+
+// const mapDispatchToProps = dispatch => ({
+//   // increment: () => dispatch(increment()),
+//   // decrement: () => dispatch(decrement()),
+//   bindActionCreators({ increment, decrement }, dispatch);
+// });
+const mapDispatchToProps = { increment, decrement };
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
---
+++ ex09_redux_example/src/store/modules/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import counter from './counter';
+import waiting from './waiting';
+
+// redux의 내장함수인 combineReducers로 리듀서를 하나로 합치는 작업을 함.
+// 여러개로 나뉘어진 리듀서 : 서브리듀서
+// 하나로 합쳐진 리듀서 : 루트 리듀서
+export default combineReducers({
+  counter,
+  waiting,
+});
