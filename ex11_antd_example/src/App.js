b2ab0095f6f597c5ba8238e509d4c0d66fdf3042
Soobinnn <isb9082@naver.com>
2020-02-16 18:12:16 +0900

    add TableSearch

--- ex11_antd_example/src/App.js
+++ ex11_antd_example/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css';
 import Header from './View/Header/Header';
-import TestResult from './View/Main/Testresult';
+import TableSearch from './Components/Table/TableSearch';
 
 function App() {
 	return (
 		<div className='App'>
 			<Layout style={{ minHeight: '100vh' }}>
 				<Header />
-				<TestResult />
+				{/* <TestResult /> */}
+				<TableSearch/>
 			</Layout>
 		</div>
 	);
---
+++ ex11_antd_example/src/Components/Table/TableSearch.js
@@ -0,0 +1,128 @@
+import React,{useState} from 'react';
+import { Table, Input, Button, Icon } from 'antd';
+
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Joe Black',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Jim Green',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+  },
+];
+
+
+ 
+const TableSearch = () => {
+  const [state, onState] = useState({
+    searchText:'',
+    searchedColumn:''
+  });
+
+  const getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            Input.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => Input.searchInput.select());
+      }
+    },
+    render: text =>
+      state.searchedColumn === dataIndex ? (
+        text
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    onState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  const handleReset = clearFilters => {
+    clearFilters();
+    onState({ searchText: '' });
+  };
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: '30%',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      width: '20%',
+      ...getColumnSearchProps('age'),
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      ...getColumnSearchProps('address'),
+    },
+  ];
+
+    return (<Table columns={columns} dataSource={data} />);
+}
+
+
+export default TableSearch;
