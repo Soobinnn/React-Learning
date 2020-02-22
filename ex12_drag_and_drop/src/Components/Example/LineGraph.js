585cc95cf05649a4122f39b0776cae2482dd8dca
Soobinnn <isb9082@naver.com>
2020-02-22 19:54:45 +0900

    add

--- ex12_drag_and_drop/src/Components/Example/LineGraph.js
+++ ex12_drag_and_drop/src/Components/Example/LineGraph.js
@@ -7,7 +7,28 @@ const data = {
     {
       label: 'My First dataset',
       fill: false,
-      lineTension: 0.1,
+      lineTension: 0,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'red',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    },
+    {
+      label: 'My First dataset2',
+      fill: false,
+      lineTension: 0,
       backgroundColor: 'rgba(75,192,192,0.4)',
       borderColor: 'rgba(75,192,192,1)',
       borderCapStyle: 'butt',
@@ -23,7 +44,7 @@ const data = {
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
+      data: [45, 19, 70, 61, 46, 50, 40]
     }
   ]
 };
